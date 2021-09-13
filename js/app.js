2a128819a0924d7a994f36a57bb38deb7f4b8712
Портфолио <80357121+Islam1595@users.noreply.github.com>
2021-09-13 14:57:30 +0300

    Кинопоиск Api

---
+++ js/app.js
@@ -0,0 +1,66 @@
+const API_KEY = "50cffacb-da6b-4487-a6a9-e2eab5368132";
+const API_URL_POPULAR = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1';
+const API_URL_SEARCH = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=';
+
+getMovies(API_URL_POPULAR);
+
+async function getMovies(url) {
+   const resp = await fetch(url, {
+      headers: {
+         "Content-Type": "application/json",
+         "X-API-KEY": API_KEY,
+      },
+   });
+   const respData = await resp.json();
+   showMovies(respData);
+}
+
+ function getClassByRata(vote) {
+    if (vote >= 7) {
+       return "green";
+    } else if (vote >= 5) {
+       return "orange";
+    } else {
+       return "red";
+    }
+}
+
+
+function showMovies(data) {
+   const moviesEl = document.querySelector(".movies");
+
+   //Очищаем предедущие фильмы
+   document.querySelector(".movies").innerHTML = "";
+
+   data.films.forEach((movie) => {
+      const movieEl = document.createElement("div");
+      movieEl.classList.add("movie");
+      movieEl.innerHTML = `
+      <div class="movie__inner">
+         <img src="${movie.posterUrlPreview}" class="movie__image" alt="movie">
+         <div class="movie__darkned"></div>
+      </div> 
+      <div class="movie__info"> 
+      <div class="movie__title">${movie.nameRu}</div>
+      <div class="movie__category"> ${movie.genres.map((genre) => `${genre.genre}`)} </div>
+      ${movie.rating && (`
+      <div class="movie__avarage movie__avarage-${getClassByRata(movie.rating)}">${movie.rating}</div>
+      `)
+   }
+      </div>
+      `;
+      moviesEl.appendChild(movieEl);
+      });
+}
+
+const form = document.querySelector('form');
+const search = document.querySelector('.header__search');
+
+form.addEventListener('submit', (e) => {
+   (e).preventDefault();
+
+   const apiSearchUrl = `${API_URL_SEARCH}${search.value}`;
+   if (search.value) {}
+   getMovies(apiSearchUrl);
+   search.value = "";
+});
